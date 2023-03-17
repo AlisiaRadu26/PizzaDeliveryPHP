72ecd4e36942b933da481cea744d544d24ea60b0
AlisiaRadu26 <117165690+AlisiaRadu26@users.noreply.github.com>
2023-03-17 20:50:11 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 //data credentials
 $server = 'eu-cdbr-west-03.cleardb.net';
 $user = 'b6ed5024df6013';
-$password = '8654d565';
+$pass = '8654d565';
 $database = 'heroku_6580760ffd21ba2';
 //connect to MySQL database
-$connect = new mysqli($server, $user, $password, $database) or die("Unable to connect");
+$connect = new mysqli($server, $user, $pass, $database) or die("Unable to connect");
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
 }
