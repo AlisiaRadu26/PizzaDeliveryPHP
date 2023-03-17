b46ee3ceeccfb70ef292c8fb984d30a8ffb6a089
AlisiaRadu26 <117165690+AlisiaRadu26@users.noreply.github.com>
2023-03-17 20:48:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 //data credentials
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'pizza_delivery';
+$server = 'eu-cdbr-west-03.cleardb.net';
+$user = 'b6ed5024df6013';
+$password = '8654d565';
+$database = 'heroku_6580760ffd21ba2';
 //connect to MySQL database
 $connect = new mysqli($server, $user, $password, $database) or die("Unable to connect");
 if (!$connect) {
