c1e5f822be1aa9a956d02014d0c18a4d470730ea
AlisiaRadu26 <117165690+AlisiaRadu26@users.noreply.github.com>
2023-03-19 11:05:24 +0200

    Update delete_orders.php

--- crud/delete_orders.php
+++ crud/delete_orders.php
@@ -38,6 +38,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     <meta charset="UTF-8">
     <title>Delete Order</title>
     <link rel="stylesheet" href="../css/style_delete.css?version=2">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/258a11fd7c.js" crossorigin="anonymous"></script>
 </head>
 <body>
